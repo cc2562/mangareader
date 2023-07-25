040b08e26b12b6790a9dbf6dc883e393af564e07
CCRICE <2371092140@qq.com>
2023-07-25 11:55:06 +0800

    add my page

--- lib/home/my.dart
+++ lib/home/my.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class myview extends StatefulWidget {
   const myview({super.key});
@@ -10,6 +11,33 @@ class myview extends StatefulWidget {
 class _myviewState extends State<myview> {
   @override
   Widget build(BuildContext context) {
-    return Text('1223');
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("设置"),
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              title: Text("软件版本"),
+              subtitle: Text("1.0.5"),
+              leading: Icon(Icons.archive),
+            ),
+            ListTile(
+              title: Text("仓库地址"),
+              subtitle: Text("https://github.com/cc2562/mangareader"),
+              leading: Icon(Icons.open_in_new_outlined),
+            ),
+            ListTile(
+              title: Text("软件作者"),
+              subtitle: Text("CC米饭"),
+              leading: Icon(Icons.person),
+            ),
+            ListTile(
+              title: Text("米饭的博客"),
+              subtitle: Text("https://www.ccrice.com"),
+              leading: Icon(Icons.link),
+            ),
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -70,20 +70,6 @@ class _Home_TopState extends State<Home_Top> {
                   selectedColor: Colors.purple,
                 ),
 
-                /// Likes
-                SalomonBottomBarItem(
-                  icon: Icon(Icons.favorite_border),
-                  title: Text("Likes"),
-                  selectedColor: Colors.pink,
-                ),
-
-                /// Search
-                SalomonBottomBarItem(
-                  icon: Icon(Icons.search),
-                  title: Text("Search"),
-                  selectedColor: Colors.orange,
-                ),
-
                 /// Profile
                 SalomonBottomBarItem(
                   icon: Icon(Icons.person),
